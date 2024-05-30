12d6c29bd4cbda52c101b34533eab78015fd8c7b
sara-bizar <100480962+sara-bizar@users.noreply.github.com>
2024-05-30 17:29:45 +0330

    Update game_objects.py
    1. i removed the extra list comprehension that didnt do anything 
    2.changed some names to make them more lodgical

--- game_objects.py
+++ game_objects.py
@@ -9,13 +9,14 @@ class game:
         self.colors={"Orange":"#ee541a","Blue":"#1a6dec","Yellow":"#f3d332","Purple":"#b244d3","Pink":"#e6869a","Green":"#52d734","red":"#ea2e35"}
 
         self.colorWords=["Orange","Blue","Yellow","Purple","Pink","Green","red",]
-        self.realText=random.choice(self.colorWords)
         
-        self.textColor=random.choice([i for i in self.colorWords  ])
+        self.textColor=random.choice(self.colorWords)    
+        self.text=random.choice( self.colorWords )
         self.bgColor=random.choice([i for i in self.colorWords if i != self.textColor ])
         self.backScreen=tk.Frame(window,background=self.colors.get(self.bgColor))
-        self.text=tk.Label(self.backScreen,text=self.realText,font =("Calibri",90,"bold"),fg=self.colors.get(self.textColor),bg=self.colors.get(self.bgColor) )
+        self.textLable=tk.Label(self.backScreen,text=self.text,font =("Calibri",90,"bold"),fg=self.colors.get(self.textColor),bg=self.colors.get(self.bgColor) )
         self.backScreen.pack(fill ="both", expand = True)
-        self.text.place(x=300,y=200)
+        self.textLable.place(x=300,y=200)
+
         
     
